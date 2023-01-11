50c4b01d75c63ca72bccb1e2b204164ee0eb757f
Daniel Brai <braidaniel2001@gmail.com>
2023-01-11 01:28:43 +0100

    Enabled loading of .env file

--- ecommercedrf/settings/base.py
+++ ecommercedrf/settings/base.py
@@ -2,16 +2,18 @@
 Django settings for ecommercedrf project.
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+# Load .env
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xrf%rn#5!v3e3*j%xl(pcn^6^52!gvr%49+@41aon-s*-(2#j3'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = os.environ.get("DEBUG")
 
 # Application definition
 
