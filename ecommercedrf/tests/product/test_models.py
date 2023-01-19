aa5a477b9f56c44b4767638bea6dc61cf3eea16c
Daniel Brai <braidaniel2001@gmail.com>
2023-01-19 03:04:37 +0100

    Refactored codebase and Impl. E2E tests

--- ecommercedrf/tests/product/test_models.py
+++ ecommercedrf/tests/product/test_models.py
@@ -10,7 +10,7 @@ pytestmark = pytest.mark.django_db
 class TestCategoryModel:
     def test_str_method(self, category_factory):
         # Arrange and Act - create dynamic data using factory
-        factory_obj = category_factory()
+        factory_obj = category_factory(name="test_category")
         # Assert
         assert factory_obj.__str__() == "test_category"
 
@@ -18,7 +18,7 @@ class TestCategoryModel:
 class TestBrandModel:
     def test_str_method(self, brand_factory):
         # Arrange and Act - create dynamic data using factory
-        factory_obj = brand_factory()
+        factory_obj = brand_factory(name="test_brand")
         # Assert
         assert factory_obj.__str__() == "test_brand"
 
@@ -26,6 +26,6 @@ class TestBrandModel:
 class TestProductModel:
     def test_str_method(self, product_factory):
         # Arrange and Act - create dynamic data using factory
-        factory_obj = product_factory()
+        factory_obj = product_factory(name="test_product")
         # Assert
         assert factory_obj.__str__() == "test_product"
